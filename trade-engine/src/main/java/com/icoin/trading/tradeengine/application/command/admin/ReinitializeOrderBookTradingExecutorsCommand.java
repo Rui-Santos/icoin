cc5aac3f7134c2f933ab56db3b713c9685f48d0d
liougehooa <liougehooa@users.noreply.github.com>
2014-01-13 12:22:56 +0800

    create reinit trade executors cmd
    this Command for reinitialize trade executors when orderbooks re-created

---
+++ trade-engine/src/main/java/com/icoin/trading/tradeengine/application/command/admin/ReinitializeOrderBookTradingExecutorsCommand.java
@@ -0,0 +1,11 @@
+package com.icoin.trading.tradeengine.application.command.admin;
+
+/**
+ * Created with IntelliJ IDEA.
+ * User: jihual
+ * Date: 12/27/13
+ * Time: 11:23 AM
+ * To change this template use File | Settings | File Templates.
+ */
+public class ReinitializeOrderBookTradingExecutorsCommand {
+}
