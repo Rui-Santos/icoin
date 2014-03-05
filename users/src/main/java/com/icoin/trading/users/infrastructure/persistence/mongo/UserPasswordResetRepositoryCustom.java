6eee1b6cae901f700da66e1cd6c2f89260083aa2
liougehooa <liougehooa@users.noreply.github.com>
2014-03-05 16:52:02 +0800

    remove json query code

--- users/src/main/java/com/icoin/trading/users/infrastructure/persistence/mongo/UserPasswordResetRepositoryCustom.java
+++ users/src/main/java/com/icoin/trading/users/infrastructure/persistence/mongo/UserPasswordResetRepositoryCustom.java
@@ -3,7 +3,6 @@ package com.icoin.trading.users.infrastructure.persistence.mongo;
 import com.homhon.base.domain.repository.GenericCrudRepository;
 import com.icoin.trading.users.domain.model.function.UserPasswordReset;
 import com.icoin.trading.users.domain.model.function.UserPasswordResetRepository;
-import org.springframework.data.mongodb.repository.Query;
 import org.springframework.data.repository.PagingAndSortingRepository;
 
 import java.util.Date;
@@ -18,14 +17,9 @@ import java.util.List;
  */
 public interface UserPasswordResetRepositoryCustom {
 
-    @Query(value = "{ 'email' : ?0 , " +
-            "'ip' : ?1 , " +
-            "'expirationDate' : {'$gte': ?2, '$lt': ?3} }, " +
-            "Sort: { 'expirationDate' : -1 }")
+
     List<UserPasswordReset> findNotExpiredByEmail(String email, String ip, Date fromDate, Date currentDate);
 
-    @Query(value = "{ 'email' : ?0 , " +
-            "'expirationDate' : {'$gte': ?1, '$lt': ?2} }, " +
-            "Sort: { 'expirationDate' : -1 }")
+
     List<UserPasswordReset> findNotExpiredByEmail(String email, Date fromDate, Date currentDate);
 }
