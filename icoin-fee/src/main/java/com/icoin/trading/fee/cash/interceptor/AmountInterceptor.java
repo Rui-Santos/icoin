a2546c0bcca257d077fb1cd85fb530ed0c648626
liougehooa <liougehooa@163.com>
2014-03-31 23:19:30 +0800

    amount interceptor amount change

--- icoin-fee/src/main/java/com/icoin/trading/fee/cash/interceptor/AmountInterceptor.java
+++ icoin-fee/src/main/java/com/icoin/trading/fee/cash/interceptor/AmountInterceptor.java
@@ -13,7 +13,7 @@ import org.joda.money.BigMoney;
  * To change this template use File | Settings | File Templates.
  */
 public class AmountInterceptor extends ProfilingInterceptor {
-    private BigMoney maxAmount = BigMoney.parse("BTC 5");
+    private BigMoney maxAmount = BigMoney.parse("BTC 10");
     private BigMoney minAmount = BigMoney.parse("BTC 0.01");
 
     @SuppressWarnings("unchecked")
@@ -21,17 +21,21 @@ public class AmountInterceptor extends ProfilingInterceptor {
     protected ValidationCode doIntercept(Invocation invocation) throws Exception {
         final InvocationContext context = invocation.getInvocationContext();
 
-        ValidationCode validationCode = maxAmount.isLessThan(context.getAmount()) ? ValidationCode.EXCEEDED_MAX_AMOUNT_PER_TIME : null;
+        final BigMoney amount = context.getAmount();
+        ValidationCode validationCode = maxAmount.isLessThan(amount) ? ValidationCode.EXCEEDED_MAX_AMOUNT_PER_TIME : null;
         if (validationCode != null) {
             return validationCode;
         }
 
-        validationCode = minAmount.isGreaterThan(context.getAmount()) ? ValidationCode.EXCEEDED_MIN_AMOUNT_PER_TIME : null;
+        validationCode = minAmount.isGreaterThan(amount) ? ValidationCode.EXCEEDED_MIN_AMOUNT_PER_TIME : null;
         return validationCode;
     }
 
-
     public void setMaxAmount(BigMoney maxAmount) {
         this.maxAmount = maxAmount;
+    }
+
+    public void setMinAmount(BigMoney minAmount) {
+        this.minAmount = minAmount;
     }
 }
