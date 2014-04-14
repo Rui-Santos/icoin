eb73b09a98bb43c1a230c3bed013f537b9e2f422
liougehooa <liougehooa@users.noreply.github.com>
2014-04-14 15:10:41 +0800

    add confirmed amount restriction
    add confirmed amount restriction

--- core-api/src/main/java/com/icoin/trading/api/fee/command/received/ConfirmReceivedFeeCommand.java
+++ core-api/src/main/java/com/icoin/trading/api/fee/command/received/ConfirmReceivedFeeCommand.java
@@ -4,6 +4,8 @@ import com.icoin.trading.api.fee.command.ConfirmFeeCommand;
 import com.icoin.trading.api.fee.domain.fee.FeeId;
 import org.joda.money.BigMoney;
 
+import javax.validation.constraints.NotNull;
+
 import java.util.Date;
 
 /**
@@ -14,10 +16,12 @@ import java.util.Date;
  * To change this template use File | Settings | File Templates.
  */
 public class ConfirmReceivedFeeCommand extends ConfirmFeeCommand<ConfirmReceivedFeeCommand> {
+    @NotNull
     private BigMoney amount;
 
     public ConfirmReceivedFeeCommand(FeeId feeId, BigMoney amount, Date confirmedDate) {
         super(feeId, confirmedDate);
+        isTrue(amount.isPositive());
         this.amount = amount;
     }
 
